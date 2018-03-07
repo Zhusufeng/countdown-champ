c635626fa8a3bfd935e097a435f6ee1aba475e8f
Lisa Gee <zhusufeng@gmail.com>
2018-03-07 10:04:58 -0800

    Fix single digits to have leading 0 with leading0 method

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -22,6 +22,10 @@ class Clock extends Component {
     setInterval(() => {this.getTimeUntil(this.props.deadline)}, 1000);
   }
 
+  leading0 (num) {
+    return num < 10 ? '0' + num : num;
+  }
+
   getTimeUntil (deadline) {
     const time = Date.parse(deadline) - Date.parse(new Date());
     const seconds = Math.floor((time/1000) % 60);
@@ -33,17 +37,17 @@ class Clock extends Component {
       hours,
       minutes,
       seconds
-    })
+    });
   }
 
   render () {
     return (
       <div>
         <div>
-          <div className="Clock-days">{this.state.days} days</div>
-          <div className="Clock-hours">{this.state.hours} hours</div>
-          <div className="Clock-minutes">{this.state.minutes} minutes</div>
-          <div className="Clock-seconds">{this.state.seconds} seconds</div>
+          <div className="Clock-days">{this.leading0(this.state.days)} days</div>
+          <div className="Clock-hours">{this.leading0(this.state.hours)} hours</div>
+          <div className="Clock-minutes">{this.leading0(this.state.minutes)} minutes</div>
+          <div className="Clock-seconds">{this.leading0(this.state.seconds)} seconds</div>
         </div>
       </div>
     );
