9c200dd2ac2e81e97fd74f19fb9460d81e79ce4d
Lisa Gee <zhusufeng@gmail.com>
2018-03-07 08:54:18 -0800

    Add wireframe structure to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class App extends Component {
   render () {
     return (
-      <div>CoutndownChamp, App Component</div>
+      <div>
+        <div>Countdown to December 25, 2018</div>
+        <div>
+          <div>14 days</div>
+          <div>30 hours</div>
+          <div>15 minutes</div>
+          <div>20 seconds</div>
+        </div>
+      </div>
     );
   }
 }
