393e3b8d62adabe6f5c15a4e048adacefb9e5f38
Lisa Gee <zhusufeng@gmail.com>
2018-03-07 09:55:48 -0800

    Add getTimeUntil & calcs for time in Clock.jsx

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -11,7 +11,17 @@ class Clock extends Component {
       seconds: 0
     }
   }
+
+  getTimeUntil (deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    const seconds = Math.floor((time/1000) % 60);
+    const minutes = Math.floor((time/1000/60) % 60);
+    const hours = Math.floor(time/(1000 * 60 * 60) % 24);
+    const days = Math.floor(time/(1000 * 60 * 60 * 24));
+  }
+
   render () {
+    this.getTimeUntil(this.props.deadline);
     return (
       <div>
         <div>
